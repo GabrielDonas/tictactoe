85343b2cbd21cf8e7467f562196c22d088f8b62b
Gabriel Donas <osradecky@gmail.com>
2021-03-05 21:38:26 +0000

    create reset and restart options

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,7 +1,9 @@
 "use strict";
-
+//Create local storages and update the scores
 let gameContainer = document.getElementById("gameboard-container");
 let startScreen = document.getElementById("start-screen");
+let scoreDisplay = document.getElementById("score");
+
 let playerOneInput = document.getElementById("player1");
 let playerTwoInput = document.getElementById("player2");
 let playerOneContainer = document.getElementById("player1-container");
@@ -9,38 +11,77 @@ let playerTwoContainer = document.getElementById("player2-container");
 let playerOne;
 let playerTwo;
 
+let gameOverScreen = document.getElementById("gameover-screen");
+let restartButton = document.getElementById("restart-button");
+let resetButton = document.getElementById("reset-button");
+
 const Gameboard = (() => {
-  const _gameboard = [];
+  let _gameboard = [];
 
   const start = () => {
-    gameContainer.style.display = "none";
-    playerOneInput.addEventListener("keyup", (event) => {
-      if (event.keyCode === 13) {
-        event.preventDefault();
-        playerOneInput.disabled = true;
-        playerTwoContainer.style.display = "block";
-        playerTwoInput.focus();
-        playerOne = Player(event.target.value);
-      }
-    });
+    if (playerOne && playerTwo) {
+      gameContainer.style.display = "flex";
+    } else {
+      gameContainer.style.display = "none";
+      gameOverScreen.style.display = "none";
+      playerOneInput.addEventListener("keyup", (event) => {
+        if (event.keyCode === 13) {
+          event.preventDefault();
+          playerOneInput.disabled = true;
+          playerTwoContainer.style.display = "block";
+          playerTwoInput.focus();
+          playerOne = Player(event.target.value, 1);
+        }
+      });
 
-    playerTwoInput.addEventListener("keyup", (event) => {
-      if (event.keyCode === 13) {
-        event.preventDefault();
-        playerTwo = Player(event.target.value);
-        startScreen.style.display = "none";
-        gameContainer.style.display = "flex";
-      }
+      playerTwoInput.addEventListener("keyup", (event) => {
+        if (event.keyCode === 13) {
+          event.preventDefault();
+          playerTwo = Player(event.target.value, 2);
+          startScreen.style.display = "none";
+          gameContainer.style.display = "flex";
+          render();
+        }
+      });
+    }
+
+    for (let i = 0; i < 9; i++) {
+      _gameboard.push("");
+    }
+  };
+
+  const gameOver = () => {
+    gameOverScreen.style.display = "block";
+    restartButton.addEventListener("click", () => {
+      gameOverScreen.style.display = "none";
+      _gameboard = [];
+      controller.resetTurn();
+      start();
+      render();
+    });
+    resetButton.addEventListener("click", () => {
+      //clear Local Storage
+      location.reload();
     });
   };
 
   const render = () => {
     gameContainer.innerHTML = "";
+    scoreDisplay.innerHTML = "";
 
-    for (let i = 0; i < 9; i++) {
-      _gameboard.push("");
-    }
+    //Players Render
+    const renderPlayer = (player) => {
+      let playerName = document.createElement("div");
+      playerName.innerHTML = `${player.getName()}(0)`;
+      scoreDisplay.appendChild(playerName);
+      if (!player.getPosition()) {
+        playerName.style.color = "rgb(0, 255, 0)";
+      }
+    };
+    renderPlayer(playerOne);
+    renderPlayer(playerTwo);
 
+    //Gameboard Render
     for (const i in _gameboard) {
       let square = document.createElement("div");
       switch (_gameboard[i]) {
@@ -55,6 +96,7 @@ const Gameboard = (() => {
         default:
           square.classList.add("blank-square", "square");
           square.onclick = () => {
+            controller.addTurn();
             _gameboard[i] =
               controller.getTurn() === "firstPlayerTurn" ? "X" : "O";
             render();
@@ -65,20 +107,33 @@ const Gameboard = (() => {
     }
     controller.checkResults(_gameboard);
   };
-  return { render, start };
+  return { render, start, gameOver };
 })();
 
-const Player = (name) => {
+const Player = (name, position) => {
   const getName = () => name;
+  const getPosition = () => {
+    console.log();
+    let turn = controller.getTurn() === "firstPlayerTurn" ? 1 : 2;
+    return turn === position ? true : false;
+  };
 
-  return { getName };
+  return { getName, getPosition };
 };
 
 const controller = (() => {
   let _turn = 0;
+
   const getTurn = () => {
-    _turn++;
     return _turn & 1 ? "firstPlayerTurn" : "secondPlayerTurn";
+  };
+
+  const addTurn = () => {
+    _turn++;
+  };
+
+  const resetTurn = () => {
+    _turn = 0;
   };
 
   const _checkPosition = (arr) => {
@@ -99,8 +154,10 @@ const controller = (() => {
         if (!arr[winningCombos[i][j]]) {
           break;
         } else if (arr[winningCombos[i][j]] && j === lastSquare) {
-          //document.getElementsByClassName("square").style.color = "blue";
-          alert(" winner");
+          //which one is true here, it's the winner:
+          //console.log("player1 " + playerOne.getPosition());
+          //console.log("player2 " + playerTwo.getPosition());
+          Gameboard.gameOver();
         }
       }
     }
@@ -120,8 +177,7 @@ const controller = (() => {
     _checkPosition(oPositions);
   };
 
-  return { getTurn, checkResults };
+  return { getTurn, checkResults, resetTurn, addTurn };
 })();
 
 Gameboard.start();
-Gameboard.render();
